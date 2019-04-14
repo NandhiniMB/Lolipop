0478a4404ed557c98ec87f3c8c003adb4f37c85b
NandhiniMB <38998341+NandhiniMB@users.noreply.github.com>
2019-04-14 12:17:24 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -5,24 +5,98 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+         <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+     
+      
+         <div className="looking-for-a-lollip">
+           Looking for lollipop Alternative
+         </div>
+        
+         <div className="body">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+         </div>
+
+         <div className="primary">
+         <p className="cta-text">Primary CTA</p>
+         </div>
+         <div className="outline">
+         <p className="cta-text1">Secondary CTA</p>
+         </div>
+        
+         <div className="image1">
+         <img src={require('./img2.png')} />
+         </div>
+         
+         <div className="compare-pricing-toda">
+         <p>Compare pricing today!</p>   
+<p className="lorem-ipsum-is-simpl ">Lorem Ipsum is simply dummy text of the printing and typesetting.</p> 
+</div>  
+<div className="serving25"><p>25 Serving</p>
+<div className="serving50"><p>50 Serving</p>   
+<div className="serving100"><p>100 Serving</p>    
+<div className="serving200"><p>200 Serving</p> 
+</div>
+</div>
+</div>
+</div>
+
+<div>
+<p className="annual">Annual</p>
+<p className="monthly">Monthly</p>
+</div>
+<div className="cards">
+<div className="card">
+  <div class="container">
+    <h4><b>lollipop</b></h4> 
+    <p>$100 per month</p>
+    <p>for 100 serving</p> 
+  </div>
+  </div>
+  <div className="card">
+  <div class="container">
+    <h4><b>lollipop</b></h4> 
+    <p>$100 per month</p>
+    <p>for 100 serving</p> 
+  </div>
+  </div>
+  <div className="card">
+  <div class="container">
+    <h4><b>lollipop</b></h4> 
+    <p>$100 per month</p>
+    <p>for 100 serving</p> 
+  </div>
+  </div>
+  </div>
+
+
+
+
+<p className="heres-what-our-cust">Here’s what our customers have to say </p>
+<p className="lorem-ipsum-is-simple1 ">Lorem Ipsum is simply dummy text of the <br/>printing and typesetting industry. 
+Lorem Ipsum <br/>has been the industry's standard dummy text<br/> ever since the 1500s.</p>
+<p className="raja-ram">Raja Ram</p>
+<p className="tip-world">Tip World</p>
+
+
+<p className="loved-by-users-reco-copy-2">Loved by users,<br/> recommended by experts</p>
+<p className="lorem-ipsum-is-simple ">Lorem Ipsum is simply dummy text of the<br/> printing and typesetting industry. 
+Lorem Ipsum <br/>has been the industry's standard dummy text <br/>ever since the 1500s.</p>
+<div className="primary1">
+         <p className="cta-text">Primary CTA</p>
+</div>
+</header>
+</div>
+/*<img src="Group 4.png" className="rectangle" alt="ThirdImage"/>
+</div>
+</header>
+<br>
+</br><br></br><br></br><br></br>
+</div>*/
     );
   }
 }
 
 export default App;
+
